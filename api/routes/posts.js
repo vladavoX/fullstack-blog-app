85cddada667e8fec8083d4cc20bbd6194f3a6cb9
Vladimir Aleksic <vlada.pzs@hotmail.com>
2022-10-11 19:38:28 +0200

    Show all posts on Home page

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,5 +1,12 @@
 import express from 'express';
+import { createPost, deletePost, getPost, getPosts, updatePost } from '../controllers/post.js';
 
 const router = express.Router();
+
+router.get("/", getPosts)
+router.get("/:id", getPost)
+router.post("/", createPost)
+router.delete("/:id", deletePost)
+router.put("/:id", updatePost)
 
 export default router
