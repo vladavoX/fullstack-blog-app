559816a65ae1564279974e03cb6521a51c46cc93
Vladimir Aleksic <vlada.pzs@hotmail.com>
2022-10-13 22:05:35 +0200

    Create & Update Post

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 import express from "express"
 import cookieParser from "cookie-parser"
+import multer from "multer"
 
 import authRoutes from './routes/auth.js'
 import userRoutes from './routes/users.js'
@@ -9,6 +10,22 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, '../client/public/uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now()+file.originalname)
+  }
+})
+
+const upload = multer({ storage })
+app.post('/api/upload', upload.single('file'), function (req, res) {
+  const file = req.file
+  res.status(200).json(file.filename)
+})
+
 app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
 app.use("/api/posts", postRoutes)
--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -38,10 +38,15 @@ const Single = () => {
     }
   }
 
+  const getText = (html) => {
+    const doc = new DOMParser().parseFromString(html, "text/html")
+    return doc.body.textContent
+  }
+
   return (
     <div className='single'>
       <div className="content">
-        <img src={post?.img} alt="" />
+        <img src={`../uploads/${post?.img}`} alt="" />
         <div className="user">
           {post?.userImg &&
             <img src={post.userImg} alt="" />
@@ -52,14 +57,14 @@ const Single = () => {
           </div>
           {currentUser.username === post.username &&
             <div className="edit">
-              <Link to={`/write?edit=2`}>
+              <Link to={`/write?edit=2`} state={post}>
                 <img src={Edit} alt="" />
               </Link>
               <img onClick={handleDelete} src={Delete} alt="" />
             </div>}
         </div>
         <h1>{post.title}</h1>
-        {post.desc}
+        {getText(post.desc)}
       </div>
       <Menu cat={post.cat} />
     </div>
